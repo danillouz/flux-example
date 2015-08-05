8f2524ac69311a2632930ec29c88555ba669383e
Daniel Illouz <daniel.illouz@me.com>
2015-08-05 12:22:25 +0200

    Added logic to determine if item has been favorited.

--- lib/js/components/queue-item.jsx
+++ lib/js/components/queue-item.jsx
@@ -3,9 +3,25 @@
  */
 
 var React = require('react');
+var QueueAction = require('../actions/queue');
 
 // Component.
 module.exports = React.createClass({
+
+	/**
+	 * Click handler that handles favorite action.
+	 *
+	 * @param {Object} event - event Object
+	 */
+	handleFavorite: function (event) {
+		var itemId = event.currentTarget.value;
+
+		if (this.props.favorited) {
+			QueueAction.unFavoriteItem(itemId);
+		} else {
+			QueueAction.favoriteItem(itemId);
+		}
+	},
 
 	/**
 	 * Renders the virtual DOM element.
@@ -14,8 +30,12 @@ module.exports = React.createClass({
 	 */
 	render: function () {
 		return (
-			<li className="queue-data-item" data-id={ this.props.id }>
+			<li className={ 'queue-data-item' + (this.props.favorited ? ' favorited' : '') }>
 				{ this.props.name }
+
+				<span className="fav-button" onClick={ this.handleFavorite } value={ this.props.id }>
+					<i className={ 'fa' + (this.props.favorited ? ' fa-star' : ' fa-star-o') } />
+				</span>
 			</li>
 		);
 	}
